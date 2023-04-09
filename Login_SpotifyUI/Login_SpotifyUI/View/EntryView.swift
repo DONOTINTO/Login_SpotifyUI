04d07d9800fc44652340d0d0982b0cd961f8effc
DONOTINTO <goods99j@gmail.com>
2023-04-09 21:31:19 +0900

    [add] EntryView 파일 추가

---
+++ Login_SpotifyUI/Login_SpotifyUI/View/EntryView.swift
@@ -0,0 +1,20 @@
+//
+//  EntryView.swift
+//  Login_SpotifyUI
+//
+//  Created by 이중엽 on 2023/04/09.
+//
+
+import UIKit
+
+class EntryView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
