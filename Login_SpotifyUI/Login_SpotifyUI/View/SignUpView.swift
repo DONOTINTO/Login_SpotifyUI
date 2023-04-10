bac5bbb233b851ef0d0671ea10687a7d4eab40e9
DONOTINTO <goods99j@gmail.com>
2023-04-10 22:01:41 +0900

    [add] SignUpView 추가

---
+++ Login_SpotifyUI/Login_SpotifyUI/View/SignUpView.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpView.swift
+//  Login_SpotifyUI
+//
+//  Created by 이중엽 on 2023/04/10.
+//
+
+import UIKit
+
+class SignUpView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
