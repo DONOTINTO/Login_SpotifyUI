cd0fecd04ea3afd623010aa86a7f2037174c0cd8
DONOTINTO <goods99j@gmail.com>
2023-04-11 23:32:58 +0900

    [add] LoginVC 파일 추가

---
+++ Login_SpotifyUI/Login_SpotifyUI/Controller/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  Login_SpotifyUI
+//
+//  Created by 이중엽 on 2023/04/11.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
