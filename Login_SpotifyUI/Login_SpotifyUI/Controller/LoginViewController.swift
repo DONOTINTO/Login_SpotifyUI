eff1813e9153d6bf9af4873083ec66cb37a374ae
DONOTINTO <goods99j@gmail.com>
2023-04-11 23:58:57 +0900

    [update] LoginView 추가 및 설정

--- Login_SpotifyUI/Login_SpotifyUI/Controller/LoginViewController.swift
+++ Login_SpotifyUI/Login_SpotifyUI/Controller/LoginViewController.swift
@@ -6,11 +6,26 @@
 //
 
 import UIKit
+import SnapKit
 
 class LoginViewController: UIViewController {
-
+    let loginView = LoginView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        initialSetup()
+        makeUI()
+    }
+    
+    func initialSetup() {
+        self.view.addSubview(loginView)
+        self.title = "로그인하기"
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+    }
+    
+    func makeUI() {
+        loginView.snp.makeConstraints {
+            $0.edges.equalTo(self.view.safeAreaLayoutGuide.snp.edges)
+        }
     }
 }
