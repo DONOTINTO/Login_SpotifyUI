1fa7f1f2076fab3e78e243ce34f3bea758b357a3
DONOTINTO <goods99j@gmail.com>
2023-04-11 23:37:12 +0900

    [add] LoginView 파일 추가

---
+++ Login_SpotifyUI/Login_SpotifyUI/View/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  Login_SpotifyUI
+//
+//  Created by 이중엽 on 2023/04/11.
+//
+
+import UIKit
+
+class LoginView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+}
