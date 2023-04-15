760c8d9539172d5a1302b8a7bb156907206548c9
DONOTINTO <goods99j@gmail.com>
2023-04-15 12:35:59 +0900

    [add] SuccessAlertView 파일 생성

---
+++ Login_SpotifyUI/Login_SpotifyUI/View/SuccessAlertView.swift
@@ -0,0 +1,24 @@
+//
+//  SuccessAlertView.swift
+//  Login_SpotifyUI
+//
+//  Created by 이중엽 on 2023/04/15.
+//
+
+import UIKit
+
+class SuccessAlertView: UIView {
+    
+    let logoImage = UIImageView()
+    let welcomeLabel = UILabel()
+    let confirmButton = UIButton()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+}
