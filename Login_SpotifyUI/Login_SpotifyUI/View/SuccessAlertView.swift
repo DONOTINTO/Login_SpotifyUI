2a0e1e24d14d836e5bafa7740d534c153a82f8d7
DONOTINTO <goods99j@gmail.com>
2023-04-15 12:51:41 +0900

    [update] 제약조건 추가

--- Login_SpotifyUI/Login_SpotifyUI/View/SuccessAlertView.swift
+++ Login_SpotifyUI/Login_SpotifyUI/View/SuccessAlertView.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class SuccessAlertView: UIView {
     
@@ -20,5 +21,30 @@ class SuccessAlertView: UIView {
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
-    
+ 
+    func makeUI() {
+        self.snp.makeConstraints {
+            $0.height.equalToSuperview().dividedBy(3)
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.centerX.centerY.equalToSuperview()
+        }
+        
+        logoImage.snp.makeConstraints {
+            $0.top.equalTo(self.snp.top).offset(39)
+            $0.centerX.equalTo(self.snp.centerX)
+        }
+        
+        welcomeLabel.snp.makeConstraints {
+            $0.top.equalTo(logoImage.snp.bottom).offset(39)
+            $0.centerX.equalTo(self.snp.centerX)
+        }
+        
+        confirmButton.snp.makeConstraints {
+            $0.height.equalTo(49)
+            $0.width.equalTo(134)
+            $0.top.equalTo(welcomeLabel.snp.bottom).offset(39)
+            $0.centerX.equalTo(self.snp.centerX)
+        }
+    }
 }
