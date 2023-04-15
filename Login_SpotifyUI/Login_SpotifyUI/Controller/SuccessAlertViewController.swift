70d01bfe4d8273f05a9f84e10b6bb23208fbd9b4
DONOTINTO <goods99j@gmail.com>
2023-04-15 12:21:48 +0900

    [add] SuccessAlertVC 파일 생성

---
+++ Login_SpotifyUI/Login_SpotifyUI/Controller/SuccessAlertViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SuccessAlertViewController.swift
+//  Login_SpotifyUI
+//
+//  Created by 이중엽 on 2023/04/15.
+//
+
+import UIKit
+
+class SuccessAlertViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
