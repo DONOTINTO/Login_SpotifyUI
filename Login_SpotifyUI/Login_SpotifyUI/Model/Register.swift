0f44b033577d97e7d167532cf580a6620312ff2a
DONOTINTO <goods99j@gmail.com>
2023-04-16 14:08:32 +0900

    [add] Register 모델 생성

---
+++ Login_SpotifyUI/Login_SpotifyUI/Model/Register.swift
@@ -0,0 +1,15 @@
+//
+//  Register.swift
+//  Login_SpotifyUI
+//
+//  Created by 이중엽 on 2023/04/16.
+//
+
+import UIKit
+
+struct Register {
+    let identification: String
+    let password: String
+    let phone: String
+    let keyNumber: Int
+}
