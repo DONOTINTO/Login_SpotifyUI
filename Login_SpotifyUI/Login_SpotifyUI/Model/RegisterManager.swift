50eeb69ba2dbee02fe742365b97d41a63f9d3678
DONOTINTO <goods99j@gmail.com>
2023-04-16 14:11:54 +0900

    [add] ResgisterManager 파일 생성

---
+++ Login_SpotifyUI/Login_SpotifyUI/Model/RegisterManager.swift
@@ -0,0 +1,15 @@
+//
+//  RegisterManager.swift
+//  Login_SpotifyUI
+//
+//  Created by 이중엽 on 2023/04/16.
+//
+
+import UIKit
+
+class RegisterManager {
+    
+    static let shared = RegisterManager()
+    
+    let RegisterList: [Register] = []
+}
