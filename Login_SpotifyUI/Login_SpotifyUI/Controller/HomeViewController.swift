beff3f2ffcba5c87d5ec33ac2dff8bb318dec88e
DONOTINTO <goods99j@gmail.com>
2023-04-24 21:05:13 +0900

    [add] HomeVC 생성

---
+++ Login_SpotifyUI/Login_SpotifyUI/Controller/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  Login_SpotifyUI
+//
+//  Created by 이중엽 on 2023/04/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
