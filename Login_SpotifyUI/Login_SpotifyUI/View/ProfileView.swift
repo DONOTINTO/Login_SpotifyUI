20f6d10d4a7b7aecbd1a350a4ff233a719fea47e
DONOTINTO <goods99j@gmail.com>
2023-04-24 21:43:14 +0900

    [add] ProfileView 파일 생성

---
+++ Login_SpotifyUI/Login_SpotifyUI/View/ProfileView.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileView.swift
+//  Login_SpotifyUI
+//
+//  Created by 이중엽 on 2023/04/24.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
