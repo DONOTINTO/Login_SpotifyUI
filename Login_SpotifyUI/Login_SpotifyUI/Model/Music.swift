b82d84fe70680507c088b3b22987e8d89bc6780f
DONOTINTO <goods99j@gmail.com>
2023-04-28 22:04:43 +0900

    [add] PlayList, Music 생성

---
+++ Login_SpotifyUI/Login_SpotifyUI/Model/Music.swift
@@ -0,0 +1,14 @@
+//
+//  Music.swift
+//  Login_SpotifyUI
+//
+//  Created by 이중엽 on 2023/04/28.
+//
+
+import Foundation
+
+struct Music {
+    var title: String
+    var artist: String
+    var like: Bool
+}
---
+++ Login_SpotifyUI/Login_SpotifyUI/Model/PlayList.swift
@@ -0,0 +1,12 @@
+//
+//  PlayList.swift
+//  Login_SpotifyUI
+//
+//  Created by 이중엽 on 2023/04/28.
+//
+
+import Foundation
+
+struct PlayList {
+    var list: [Music] = []
+}
