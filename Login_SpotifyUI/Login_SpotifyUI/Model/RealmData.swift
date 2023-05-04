a923cc21bd2010199b6ab5b57f162d57180c2cdb
DONOTINTO <goods99j@gmail.com>
2023-05-04 22:18:09 +0900

    [update] RealmData 파일 추가

---
+++ Login_SpotifyUI/Login_SpotifyUI/Model/RealmData.swift
@@ -0,0 +1,21 @@
+//
+//  RealmData.swift
+//  Login_SpotifyUI
+//
+//  Created by 이중엽 on 2023/05/04.
+//
+
+import RealmSwift
+import UIKit
+
+protocol RealmDataProtocol {
+    
+}
+
+class RealmData: RealmDataProtocol {
+    private let realm: Realm
+    
+    init(realm: Realm) {
+        self.realm = realm
+    }
+}
